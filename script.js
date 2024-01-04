85373dde5c4655bdf356eb2b5b4c98951f1c8471
Anshu Gupta <112775588+anshurauniyar17@users.noreply.github.com>
2024-01-04 14:37:35 +0545

    Update script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:".two",
+    start: "0% 95%",
+    end: "50% 50%",
+    scrub:true,
+    // markers:true,
+}})
+
+tl.to("#fanta",{
+    top: "120%",
+    left:"5%"
+},'orange')
+tl.to("#orange-slice",{
+    top: "160%",
+    left:"25%"
+},'orange')
+tl.to("#orange",{
+    width:"12%",
+    top: "170%",
+    left:"80%"
+},'orange')
+tl.to("#leaf1",{
+    width:"12%",
+    top: "120%",
+    left:"85%",
+    // rotate:"120deg",
+},'orange')
+tl.to("#leaf2",{
+    width:"12%",
+    top: "170%",
+    left:"0%",
+    
+},'orange')
+tl.to("#leaf3",{
+    width:"12%",
+    top: "100%",
+    left:"0%",
+    rotate:"90deg",
+},'orange')
+
+var tl2 = gsap.timeline({scrollTrigger:{
+    trigger:".three",
+    start: "0% 90%",
+    end: " 20% 50%",
+    scrub: true,
+    // markers: true
+}
+
+})
+tl2.to("#fanta",{
+    width: "30%",
+    top:"209%",
+    left:"35%"
+},'three')
+tl2.to("#leaf1",{
+    width: "13%",
+    top:"270%",
+    left:"0%",
+    // rotate:"180deg"
+},'three')
+tl2.to("#leaf2",{
+    width: "13%",
+    top:"210%",
+    left:"85%",
+    rotate:"90deg"
+},'three')
+// tl2.to("#leaf3",{
+    
+//     width: "13%",
+//     top:"270%",
+//     left:"80%",
+//     // rotate:"deg"
+// },'three')
+tl2.to("#orange-slice",{
+    width: "20vw",
+    top:"209%",
+    left:"40%"
+},'three')
